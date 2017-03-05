74c93023a973830f493402ae1d0a7ab150f39e54
T A Russell <Dune-planet@hotmail.com>
2017-03-05 11:11:40 -0500

    done

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+'use strict'
+let urls = []
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -22,14 +24,36 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+	let scan = /^https?:\/\//
+	let isUrl = function(data) {
+		if (scan.test(data) == true) return true
+		else return false;
+	}
+	
+	req.url = req.url.slice(1)
+	if (req.url.length == 0) {
+	res.end(`<b>Trey's url shortener for Free Code Camp</b> <br>
+Add \/new\/ plus a http or https complete url to the end of this site's url to receive a shortened url.  Or, paste shortened url into bar to go to that site.`);
+	} else {
+		if (req.url.slice(0, 4) == 'new/') {
+			req.url = req.url.slice(4);
+			let x = urls.length;
+			if (isUrl(req.url)) {
+			urls.push(req.url)
+			res.end(`Your url = ${req.url}, shortened url = this site's url + /${x}  
+			} else {
+				res.end(`Invalid url, please try again.`)
+			}
+			console.log(req.url)
+		} else if (req.url[0].match(/^[0-9]/)!= null) {
+			res.redirect(urls[req.url])
+		} else {
+			res.end('not in database')
+		}
+ 		
+	}
 });
 
 // error handler
